13cf0de54c9f3070be42b3b0106d2598eccdff7a
JDaniloC <jdsc@cin.ufpe.br>
2023-03-30 16:21:39 -0300

    Improve ProfileCircle

--- Foundations/Components/ProfileCircle.swift
+++ Foundations/Components/ProfileCircle.swift
@@ -10,33 +10,45 @@ import SwiftUI
 struct ProfileCircle: View {
     @State private var imagePath: String;
     @State private var profileName: String;
+    @State private var radius: CGFloat;
     
-    init(imagePath: String, profileName: String) {
+    init(_ imagePath: String,
+         profileName: String = "",
+         radius: CGFloat = 100
+    ) {
+        self.radius = radius;
         self.imagePath = imagePath;
         self.profileName = profileName;
     }
     
     var body: some View {
         VStack {
-            ZStack {
-                Image(imagePath)
-                Circle()
+            Image(imagePath)
+                .resizable()
+                .shadow(radius: 10)
+                .overlay(Circle()
                     .inset(by: 0)
-                    .stroke(Color("light-yellow"),
-                            lineWidth: 8)
+                    .stroke(
+                        Color("light-yellow"),
+                        lineWidth: radius / 10
+                    )
+            ).frame(width: radius, height: radius)
+             .padding(.bottom)
+            
+            if (profileName != "") {
+                Text(profileName)
+                    .font(.custom("Inter-Regular",
+                                  size: 13))
+                    .foregroundColor(.black)
+                    .fontWeight(.light)
             }
-                .frame(width: 100, height: 100)
-                .padding(.bottom)
-            Text(profileName)
-                .foregroundColor(.black)
-                .fontWeight(.light)
         }
     }
 }
 
 struct ProfileCircle_Previews: PreviewProvider {
     static var previews: some View {
-        ProfileCircle(imagePath: "profile",
+        ProfileCircle("profile",
                       profileName: "Example")
     }
 }
